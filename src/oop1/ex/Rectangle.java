1bb1bcd8b66da18710c78795a49ded7781d004eb
sein0121 <sseny0121@gmail.com>
2024-04-29 17:09:39 +0900

    240423 - 객체지향 문제1

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -1,18 +1,15 @@
 package oop1.ex;
 
-public class RectangleProceduralMain {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        rectangle.width=5;
-        rectangle.height=8;
-
-        int area = rectangle.calculateArea();
-        System.out.println("넓이: " + area);
-        int perimeter = rectangle.calculatePerimeter();
-        System.out.println("둘레 길이: " + perimeter);
-        boolean square = rectangle.isSquare();
-        System.out.println("정사각형 여부: " + square);
-
+public class Rectangle {
+    int width;
+    int height;
+    int calculateArea() {
+        return width * height;
     }
-
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+    boolean isSquare() {
+        return this.width == this.height;
+    }
 }
